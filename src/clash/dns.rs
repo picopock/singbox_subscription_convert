d502d60d28f385b2b6d6557a768ace993cb8c6f4
messica <picopock@163.com>
2025-06-13 23:57:59 +0800

    feat: 🎸 adapt clash config file

--- src/clash/dns.rs
+++ src/clash/dns.rs
@@ -3,20 +3,20 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct DNS {
-    pub enable: bool,
-    pub ipv6: bool,
-    pub default_nameserver: Vec<String>,
-    pub enhanced_mode: String,
-    pub fake_ip_range: String,
-    pub use_hosts: bool,
-    pub nameserver: Vec<String>,
-    pub fallback: Vec<String>,
-    pub fallback_filter: FallbackFilter,
+    pub enable: Option<bool>,
+    pub ipv6: Option<bool>,
+    pub default_nameserver: Option<Vec<String>>,
+    pub enhanced_mode: Option<String>,
+    pub fake_ip_range: Option<String>,
+    pub use_hosts: Option<bool>,
+    pub nameserver: Option<Vec<String>>,
+    pub fallback: Option<Vec<String>>,
+    pub fallback_filter: Option<FallbackFilter>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FallbackFilter {
-    pub geoip: bool,
+    pub geoip: Option<bool>,
     #[serde(rename = "ipcidr")]
-    pub ip_cidr: Vec<String>,
+    pub ip_cidr: Option<Vec<String>>,
 }
--- src/clash/mod.rs
+++ src/clash/mod.rs
@@ -7,13 +7,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct Config {
-    pub mixed_port: u16,
-    pub allow_lan: bool,
-    pub bind_address: String,
-    pub mode: Mode,
-    pub log_level: LogLevel,
-    pub external_controller: String,
-    pub dns: dns::DNS,
+    pub mixed_port: Option<u16>,
+    pub allow_lan: Option<bool>,
+    pub bind_address: Option<String>,
+    pub mode: Option<Mode>,
+    pub log_level: Option<LogLevel>,
+    pub external_controller: Option<String>,
+    pub dns: Option<dns::DNS>,
     pub proxies: Vec<proxy::Proxy>,
     pub proxy_groups: Vec<proxy_group::ProxyGroup>,
     pub rules: Vec<String>,
